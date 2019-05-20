d317966c4bb393ba5243e8b09cb9dd7084dad8f2
laceous <laceous@gmail.com>
2019-05-20 14:46:39 -0700

    order

--- Net.Laceous.Utilities/Extensions/CharExtensions.cs
+++ Net.Laceous.Utilities/Extensions/CharExtensions.cs
@@ -6,13 +6,13 @@
     public static class CharExtensions
     {
         /// <summary>
-        /// Checks if the char is a hex char (0-9, a-f, A-F)
+        /// Checks if the char is a hex char (0-9, A-F, a-f)
         /// </summary>
         /// <param name="c">Char to check</param>
         /// <returns>True if hex, false if not hex</returns>
         public static bool IsHex(this char c)
         {
-            return (c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F');
+            return (c >= '0' && c <= '9') || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f');
         }
     }
 }
